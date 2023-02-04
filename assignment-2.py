6419f8ec4bea894f77eac261d5878b9ede8d04e8
AkshatBari02 <124452239+AkshatBari02@users.noreply.github.com>
2023-02-04 23:35:47 +0530

    Day 4 Assignment submitted

---
+++ assignment-2.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    if n ==0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+print("please enter a number:")
+n=int(input("you enterd:"))
+print(factorial(n))
+
+
+
+
+
+
+
+
+
+
